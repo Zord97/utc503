770d50b7cdfbf4802179a6a142cda47840924c5d
Benjamin Chénegrin <bchenegrin@sio.local>
2020-10-07 16:40:44 +0200

    modification

--- software.php
+++ software.php
@@ -2,7 +2,7 @@
 require_once("include/datas.inc.php");
 const DATA_LOCATION='datas/';
 echo "----------------------Gestion des Licences logicielles--------------------------\n";
-echo "----------------------jc-HERON--------------------------\n";
+echo "----------------------jc-TEST--------------------------\n";
 
 
 $employees=loadAndDump(DATA_LOCATION."employees.json");
